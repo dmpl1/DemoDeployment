fa534c21caf06c0bf495eedcafad7dcac48aee2d
dimple <dimple@pkglobal.com>
2021-05-07 14:09:55 +0530

    added test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,7 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Is returning 4 when adding 2 + 3', () => {
+  expect(2 + 3).toBe(5);
+});
