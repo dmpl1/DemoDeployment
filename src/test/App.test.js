7b07e1134f309b19e8924d0ee2afa42edb8be9ce
dimple <dimple@pkglobal.com>
2021-05-07 15:30:06 +0530

    added test folder

---
+++ src/test/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Is returning 4 when adding 2 + 3', () => {
+  expect(2 + 3).toBe(5);
+});
